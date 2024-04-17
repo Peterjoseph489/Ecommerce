045d679fd75d4ec0efbccbf2259dbe659a72b3e6
Peter Joseph <josephpeter489@gmail.com>
2024-04-17 12:59:07 +0100

    working on Models

--- Models/productModel.js
+++ Models/productModel.js
@@ -4,7 +4,26 @@ const productModel = new mongoose.Schema({
         type: String,
         // required: [true, "Product Name is required"]
     },
-    ProductDescription: {
-        
-    }
-})
+    ProductDescription:{
+        type:String,
+        // required:[true,"product image is required"]
+    },
+    Price:{
+        type:Number,
+        // required:[true,"product description is required"]
+    },
+    ProductSize:{
+        type:String,
+        // required:[true,"product size is required"]
+    },
+    ProductImage:{
+        type:String
+     },
+     Quantity:{
+        type:Number,
+        default:0
+     }
+},{timestamps:true})
+
+const ProductModel = mongoose.model("order",productModel)
+module.exports=ProductModel
